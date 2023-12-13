36de08ef36ebe03086b7048a4d32584e9c298008
Mark <fqchenxin@gmail.com>
2023-12-13 23:24:14 +1100

    update test case

--- src/utils/problems/hashing-approach.ts
+++ src/utils/problems/hashing-approach.ts
@@ -9,28 +9,26 @@ const starterCodeHasingApproach = `function hashingApproach(s,k){
 const handlerHasingApproach = (fn: any) => {
 	// fn is the callback that user's code is passed into
 	try {
-		const nums = [
-			[2, 7, 11, 15],
-			[3, 2, 4],
-			[3, 3],
+		const s = [
+			"amazon",
+			"abaaba"
 		];
 
-		const targets = [9, 6, 6];
+		const k = [3, 2];
 		const answers = [
-			[0, 1],
-			[1, 2],
-			[0, 1],
+			1,
+			3
 		];
 
 		// loop all tests to check if the user's code is correct
-		for (let i = 0; i < nums.length; i++) {
+		for (let i = 0; i < s.length; i++) {
 			// result is the output of the user's function and answer is the expected output
-			const result = fn(nums[i], targets[i]);
+			const result = fn(s[i], k[i]);
 			assert.deepStrictEqual(result, answers[i]);
 		}
 		return true;
 	} catch (error: any) {
-		console.log("twoSum handler function error");
+		console.log("hashingApproach handler function error");
 		throw new Error(error);
 	}
 };
